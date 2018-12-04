2a268224c5c9c0623732b34ef21ebed271ba3e0d
Jian Liew <jianloongliew@gmail.com>
2018-12-05 01:02:47 +1100

    Solved 17

--- 17.js
+++ 17.js
@@ -2,33 +2,47 @@
  * @param {string} digits
  * @return {string[]}
  */
-var letterCombinations = function(digits) {
-  const arr = (digits + "").split("");
-  let result = [];
-  let permutations = [];
 
-  for (let i = 0; i < arr.length; i++) {
-    switch (arr[i]) {
-      case "2":
-        result.push(["a", "b", "c"]);
-        break;
-      case "3":
-        result.push(["d", "e", "f"]);
-        break;
-      case "4":
-        result.push(["g", "h", "i"]);
-        break;
-      case "5":
-        result.push(["j", "k", "l"]);
-        break;
-      case "6":
-        result.push(["m", "n", "o"]);
-        break;
-      case "7":
-        result.push(["p", "q", "r", "s"]);
-        break;
-      case "8":
-        result.push(["t", "u", "v"]);
+var letterCombinations = function(digits) {
+  let numString = [
+    "",
+    "",
+    "abc",
+    "def",
+    "ghi",
+    "jkl",
+    "mno",
+    "pqrs",
+    "tuv",
+    "wxyz"
+  ];
+  let ans = [];
+  let target = [];
+  for (let i = 0; i < digits.length; i++) {
+    target.push(numString[parseInt(digits[i])]);
+  }
+
+  console.log(target);
+
+  if (target.length > 0) {
+    for (let l = 0; l < target[0].length; l++) {
+      ans.push(target[0][l]);
     }
   }
+
+  console.log(ans);
+
+  for (let j = 1; j < target.length; j++) {
+    let temp = [];
+    for (let k = 0; k < target[j].length; k++) {
+      for (let m = 0; m < ans.length; m++) {
+        temp.push(ans[m] + target[j][k]);
+      }
+    }
+    ans = temp;
+  }
+
+  return ans;
 };
+
+console.log(letterCombinations("237"));
