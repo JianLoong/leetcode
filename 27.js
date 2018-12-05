17ef8731e0de89d164ad041586e3f0088c67e68c
Jian Liew <jianloongliew@gmail.com>
2018-12-05 15:40:55 +1100

    Solved 27,66

--- 27.js
+++ 27.js
@@ -5,12 +5,29 @@
  * @param {number} val
  * @return {number}
  */
-var removeElement = function(nums, val) {
+let removeElement = function(nums, val) {
   nums.sort();
   let start = nums.indexOf(val);
   let end = nums.lastIndexOf(val);
 
   if (start != -1 && end != -1) nums.splice(start, end - start + 1);
+};
+
+/**
+ * Two pointer solution.
+ *
+ * @param {number[]} nums
+ * @param {number} val
+ */
+let removeElement = function(nums, val) {
+  let i = 0;
+  for (let j = 0; j < nums.length; j++) {
+    if (nums[j] != val) {
+      nums[i] = nums[j];
+      i++;
+    }
+  }
+  return i;
 };
 
 let nums = [2];
--- 66.js
+++ 66.js
@@ -1,12 +1,43 @@
 /**
+ * Naive solution does not work as it hits the int overflow
  * @param {number[]} digits
  * @return {number[]}
  */
-// Naive solution does not work as it hits the int overflow
-var plusOne = function(digits) {
+var plusOne = digits => {
   let value = parseInt(digits.join("")) + 1;
 
   return (value + "").split("");
 };
 
-console.log(plusOne([1, 2, 3]));
+/**
+ * Solution using manual array addition. The carry will be kept.
+ * If towards the end, the carry is 1, an element will be placed in front.
+ *
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = digits => {
+  let carry = 0;
+  for (let i = digits.length - 1; i >= 0; i--) {
+    let add = 0;
+    if (i == digits.length - 1) {
+      add = digits[i] + 1;
+    } else {
+      add = digits[i] + carry;
+    }
+
+    if (add >= 10) {
+      digits[i] = 0;
+      carry = 1;
+    } else {
+      digits[i] = add;
+      carry = 0;
+    }
+  }
+
+  if (carry == 1) digits.unshift(1);
+
+  return digits;
+};
+
+console.log(plusOne([6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3]));
