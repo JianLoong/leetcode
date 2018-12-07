d0ed7b3dfcc4178f2bd6c7726386af05a9af14e6
Jian Liew <jianloongliew@gmail.com>
2018-12-07 20:50:44 +1100

    85

--- 85.js
+++ 85.js
@@ -3,19 +3,26 @@
  * @return {number}
  */
 var maximalRectangle = function(matrix) {
+  // The rows
   const n = matrix.length;
   if (n === 0) return 0;
+  // the columns
   const m = matrix[0].length;
 
   const h = new Array(n).fill(0);
 
   let max = 0;
   for (let j = 0; j < m; j++) {
+    // This will create the height matrix.
     for (let i = 0; i < n; i++) {
-      if (matrix[i][j] === "1") h[i]++;
-      else h[i] = 0;
+      if (matrix[i][j] === "1") {
+        h[i]++;
+      } else {
+        h[i] = 0;
+      }
     }
 
+    console.log(h);
     for (let i = 0; i < n; i++) {
       let k1 = i - 1;
       while (k1 >= 0 && h[i] <= h[k1]) {
@@ -26,9 +33,7 @@ var maximalRectangle = function(matrix) {
         k2++;
       }
       max = Math.max(max, h[i] * (k2 - k1 - 1));
-      console.log(k2 - k1 - 1);
     }
-    console.log();
   }
   return max;
 };
