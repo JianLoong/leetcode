46eeaa60ad4c583340bee96804ef4bd149b1b831
Jian Liew <jianloongliew@gmail.com>
2018-12-09 21:52:33 +1100

    Solved 5

--- 5.js
+++ 5.js
@@ -2,27 +2,32 @@
  * @param {string} s
  * @return {string}
  */
-var longestPalindrome = function(s) {
-  const result = [];
-  const length = s.length;
-  let notPalindromeIndex = 0;
-  for (let i = 0; i < length; i++) {
-    const value = s.slice(0, i + 1);
-    console.log(value);
+const longestPalindrome = s => {
+  if (s == null || s.length < 1) return "";
+  let start = 0;
+  let end = 0;
+  for (let i = 0; i < s.length; i++) {
+    let len1 = expandAroundCenter(s, i, i);
+    let len2 = expandAroundCenter(s, i, i + 1);
+    let len = Math.max(len1, len2);
+    if (len > end - start) {
+      start = i - (len - 1) / 2;
+      end = i + len / 2;
+    }
   }
-
-  console.log(result);
+  return s.substring(Math.ceil(start), end + 1);
 };
 
-const isPalindrome = s => {
-  if (s.length == 0) return false;
-  if (s.length === 2) return s[0] === s[1];
-  return (
-    s ===
-    s
-      .split("")
-      .reverse()
-      .join("")
-  );
+const expandAroundCenter = (s, left, right) => {
+  let L = left;
+  let R = right;
+  while (L >= 0 && R < s.length && s.charAt(L) == s.charAt(R)) {
+    L--;
+    R++;
+  }
+  return R - L - 1;
 };
-console.log(longestPalindrome("abacdfgdcaba"));
+
+const s = "cabbad";
+
+console.log(longestPalindrome(s));
