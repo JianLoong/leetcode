2b2a9fada80583db6d5215d6955e9a7d75321473
Jian Liew <jianloongliew@gmail.com>
2018-12-10 17:50:22 +1100

    Solved 476

--- 476.js
+++ 476.js
@@ -1,11 +1,13 @@
 /**
+ * Naive solution.
+ *
  * @param {number} num
  * @return {number}
  */
 const findComplement = num => {
   const str = num.toString(2);
   let result = "";
-  for (var i of str) {
+  for (let i of str) {
     result += i === "1" ? "0" : "1";
   }
   return parseInt(result, 2);
