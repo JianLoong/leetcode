160e84ce5563cff069b2508daa73caed5501a13b
Jian Liew <jianloongliew@gmail.com>
2018-12-10 19:37:00 +1100

    Solved 500

--- 500.js
+++ 500.js
@@ -2,15 +2,40 @@
  * @param {string[]} words
  * @return {string[]}
  */
-var findWords = function(words) {
-  const firstRow = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"];
-  const secondRow = ["a", "s", "d", "f", "g", "h", "j", "k", "l"];
-  const thirdRow = ["z", "x", "c", "v", "b", "n", "m"];
+const findWords = words => {
+  const firstRow = new Set(["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"]);
+  const secondRow = new Set(["a", "s", "d", "f", "g", "h", "j", "k", "l"]);
+  const thirdRow = new Set(["z", "x", "c", "v", "b", "n", "m"]);
 
   const result = [];
+  for (let word of words) {
+    let firstRowCount = 0;
+    let secondRowCount = 0;
+    let thirdRowCount = 0;
+    for (let character of word) {
+      if (firstRow.has(character.toLowerCase())) {
+        firstRowCount++;
+      }
+      if (secondRow.has(character.toLowerCase())) {
+        secondRowCount++;
+      }
 
-  for (let word of input) {
-    console.log(word);
+      if (thirdRow.has(character.toLowerCase())) {
+        thirdRowCount++;
+      }
+    }
+
+    if (firstRowCount === word.length) {
+      result.push(word);
+    }
+
+    if (secondRowCount === word.length) {
+      result.push(word);
+    }
+
+    if (thirdRowCount === word.length) {
+      result.push(word);
+    }
   }
 
   return result;
