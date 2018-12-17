d3b3c1c53066912256e86982bfef7984f4d4395e
Jian Liew <jianloongliew@gmail.com>
2018-12-17 22:13:54 +1100

    Solved 278

---
+++ 278.js
@@ -0,0 +1,45 @@
+/**
+ * Definition for isBadVersion()
+ *
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+const max = 100;
+const badVersion = 22;
+
+function isBadVersion(value) {
+  if (value >= badVersion) return true;
+  return false;
+}
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+const solution = function(isBadVersion) {
+  /**
+   * @param {integer} n Total versions
+   * @return {integer} The first bad version
+   */
+  return function(n) {
+    let left = 1;
+    let right = n;
+    while (left < right) {
+      let mid = Math.floor(left + (right - left) / 2);
+      //console.log(mid);
+      if (isBadVersion(mid)) {
+        right = mid;
+      } else {
+        left = mid + 1;
+      }
+    }
+    return left;
+  };
+};
+
+const value = solution(isBadVersion);
+
+console.log(solution(isBadVersion)(50));
