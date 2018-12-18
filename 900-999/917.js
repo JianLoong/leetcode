f168ef39efe2bae9a358f122fe1adce1990950f7
Jian Liew <jianloongliew@gmail.com>
2018-12-18 11:13:51 +1100

    Solved 917

--- 900-999/917.js
+++ 900-999/917.js
@@ -1,7 +1,30 @@
 /**
+ * Using a "stack". (52ms)
+ *
  * @param {string} S
  * @return {string}
  */
-var reverseOnlyLetters = function(S) {
-    
+const reverseOnlyLetters = S => {
+  let letters = [];
+  let regex = /[A-Za-z]/g;
+  for (let i = 0; i < S.length; i++) {
+    if (S[i].match(regex)) {
+      letters.push(S[i]);
+    }
+  }
+  let result = [];
+
+  for (let i = 0; i < S.length; i++) {
+    if (S[i].match(regex)) {
+      result.push(letters.pop());
+    } else {
+      result.push(S[i]);
+    }
+  }
+
+  return result.join("");
 };
+
+const input = "a-bC-dEf-ghIj";
+
+console.log(reverseOnlyLetters(input));
