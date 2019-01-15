9dd22a1492371e89e18a88a0c23ef2bf68b36768
Jian Liew <jianloongliew@gmail.com>
2019-01-15 12:45:36 +1100

    Added another solution for 509

--- 500-599/509.js
+++ 500-599/509.js
@@ -14,6 +14,19 @@ var fib = function(N) {
   return fib[N];
 };
 
-const input = 2;
+/**
+ * Recursive Solution. 124ms
+ *
+ * @param {number} n
+ */
+const fibRecursive = n => {
+  if (n <= 1) {
+    return n;
+  }
+  return fibRecursive(n - 1) + fibRecursive(n - 2);
+};
+
+const input = 8;
 
 console.log(fib(input));
+console.log(fibRecursive(input));
