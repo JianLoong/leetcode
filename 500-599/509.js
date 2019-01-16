437c7653b929a3686409a632ae4c81830925c459
Jian Liew <jianloongliew@gmail.com>
2019-01-16 23:07:23 +1100

    Refactored 509

--- 500-599/509.js
+++ 500-599/509.js
@@ -4,13 +4,11 @@
  * @param {number} N
  * @return {number}
  */
-var fib = function(N) {
+const fib = N => {
   let fib = [0, 1];
-
   for (let i = 2; i <= N; i++) {
     fib[i] = fib[i - 1] + fib[i - 2];
   }
-
   return fib[N];
 };
 
@@ -20,9 +18,7 @@ var fib = function(N) {
  * @param {number} n
  */
 const fibRecursive = n => {
-  if (n <= 1) {
-    return n;
-  }
+  if (n <= 1) return n;
   return fibRecursive(n - 1) + fibRecursive(n - 2);
 };
 
